0df63f48c1488ad0b3584dcdd5f1f72ab3dd490e
Donghun Ha <donghun.dev@gmail.com>
2023-12-10 23:41:09 +0900

    update main.py

--- week1/main.py
+++ week1/main.py
@@ -4,20 +4,26 @@ from emitter import Emitter
 emitter = Emitter()
 
 @emitter.on("click", lambda x: x["pos_x"] < 500)
-async def on_click_left(event):
+def on_click_left(event):
     print("CLICK LEFT")
 
 @emitter.on("click", lambda x: x["pos_x"] >= 500)
-async def on_click_right(event):
+def on_click_right(event):
     print("CLICK RIGHT")
 
 def on_click(x, y, button, pressed):
-    print(button, pressed, x, y)
     if (button == mouse.Button.right):
         print('Exiting...')
         return False
-    emitter.emit("click", pos_x= x, pos_y= y)
+    if (pressed):
+        emitter.emit("click", pos_x= x, pos_y= y)
     
 
 with mouse.Listener(on_click=lambda x,y, button, pressed: on_click(x, y, button, pressed)) as listener:
     listener.join()
+
+
+
+
+
+
