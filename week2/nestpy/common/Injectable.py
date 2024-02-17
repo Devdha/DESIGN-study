c3ab757779119d4f9347095b9247b2c947cd5620
Donghun Ha <donghun.dev@gmail.com>
2024-02-18 02:35:26 +0900

    feat: set controller and service on module

--- week2/nestpy/common/Injectable.py
+++ week2/nestpy/common/Injectable.py
@@ -2,7 +2,7 @@ from typing import Any
 
 def Injectable():
     def wrap(cls: Any) -> Any:
-        cls.__injectable = True
+        cls._is_injectable = True
         return cls
   
     return wrap
--- week2/nestpy/common/Module.py
+++ week2/nestpy/common/Module.py
@@ -1,8 +1,33 @@
 from typing import Dict, Any
 
+registered_controllers = {}
+registered_services = {}
+
 def Module(module_config: Dict[str, Any]) -> Any:
     def wrap(cls: Any) -> Any:
         cls.__module_config = module_config
+
+        service = module_config.get('provider')
+        if service and service._is_injectable:
+            registered_services[service.__name__] = service
+        else:
+            raise Exception('Service is not injectable')
+        
+        controller = module_config.get('controller')
+        if controller:
+            registered_controllers[controller.__path] = controller
+        else:
+            raise Exception('Controller is not defined')
+        
+        providers = {}
+        for name, type in controller.__init__.__annotations__.items():
+            if type._is_injectable:
+                provider = registered_services[type.__name__]
+                if not provider:
+                    raise Exception(f'Provider {name} is not defined')
+                providers[name] = provider
+        
+        registered_controllers[controller.__path] = controller(**providers)
         return cls
 
     return wrap
